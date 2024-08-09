f91491a0a01f58ed30205b4f369b029040b24759
temyl <mercytemi02@gmail.com>
2024-08-09 12:12:45 +0100

    updated codemania contact form

--- controller.js
+++ controller.js
@@ -1,10 +1,27 @@
-const nodemailer = require('nodemailer');
+const { transporter } = require('./nodemailer'); 
 
 const contact_form = async (req, res) => {
-    const { Name, Email, Phone, Project, details} = req.body;
-    if (!Name || !Email ) {
+    const { Name, Email, Phone, Project_details } = req.body;
+
+    if (!Name || !Email) {
         return res.status(400).json({
-            error: 'please fill blank'
+            error: 'Please fill blanks'
         });
     }
+
+    try {
+        await transporter.sendMail({
+            from: Email,
+            to: process.env.mail_username, 
+            subject: `Contact form submission from ${Name}`,
+            text: `Name: ${Name}\nEmail: ${Email}\nPhone: ${Phone}\nProject Details: ${Project_details}`
+        });
+
+        res.status(200).json({ message: 'Form submitted successfully!' });
+    } catch (error) {
+        console.error('Error sending email:', error.message);
+        res.status(500).json({ error: 'Server error' });
+    }
 };
+
+module.exports = { contact_form };
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const nodemailer = require('nodemailer');
+const { contact_form } = require('./controller');
+const router = express.Router();
+
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyparser.urlencoded({ extended: false}));
 app.use(bodyparser.json());
+
+router.post('/contact_form', contact_form);
+app.use(router);
+
+
+
+// app.post('/contact_form', contact_form);
+
 
 
 app.listen(port, () => {
